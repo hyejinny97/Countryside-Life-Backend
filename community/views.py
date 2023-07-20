cd512bbc8cb34c25b7594aa7406d05c040c04f1d
hyejin <yhj970102@gmail.com>
2023-07-20 23:06:59 +0900

    ArticleViewSet에 필터링, 정렬 기능 추가

--- community/views.py
+++ community/views.py
@@ -1,14 +1,12 @@
 from django.db.models.query import prefetch_related_objects
+from django.db.models import Count
 from .models import Article, Comment
 from .serializers import ArticleSerializer, CommentSerializer, LikeSerializer, serializers
 from .pagination import PageNation
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
-from rest_framework import permissions
-from rest_framework import status
+from rest_framework import viewsets, permissions, status, filters
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView, ListAPIView
-from rest_framework.mixins import ListModelMixin
 
 
 # 게시물 생성/조회/수정/삭제
@@ -17,6 +15,47 @@ class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly,]
     pagination_class = PageNation
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['content', 'title']
+    # ordering_fields = ['likes', 'created_at']
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # 요청에서 필터링 매개변수 가져오기
+        category = self.request.query_params.get('category', None)
+        search = self.request.query_params.get('search', None)
+        region = self.request.query_params.get('region', None)
+        
+        # 필터링 적용
+        if category:
+            queryset = queryset.filter(category=category)
+        # elif search:
+        #     queryset = queryset.filter(content__icontains=search)
+        #     queryset = queryset.filter(title__icontains=search)
+
+        if region:
+            queryset = queryset.filter(region=region)
+        
+        # 정렬 적용
+        ordering = self.request.query_params.get('ordering', 'created_at')
+        if ordering == 'created_at':
+            queryset = queryset.order_by('-created_at', '-id')
+        if ordering == 'likes':
+            queryset = queryset.annotate(likes_count=Count('like_users')).order_by('-likes_count')
+
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
